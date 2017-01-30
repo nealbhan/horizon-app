667e1ffd068b2a2128da192c80cf5c5572028aa5
neal <nebhandari@vassar.edu>
2017-01-30 14:25:46 -0800

    updated changes

--- app.js
+++ app.js
@@ -1,13 +1,21 @@
+// var args = process.argv;
+// var flagsPresent = [];
+
+// args.forEach(function(item) {
+// 	if(item === '--sum' || item === '--product') {
+// 		flagsPresent.push(item);
+// 	}
+// })
+
+var program = require('commander');
 var args = process.argv;
-var flagsPresent = [];
 
-args.forEach(function(item) {
-	if(item === '--sum' || item === '--product') {
-		flagsPresent.push(item);
-	}
-})
+program 
+	.option('-s, --sum', 'sum')
+	.option('-p, --product', 'product')
+	.parse(process.argv)
 
-if(flagsPresent.length !== 1) {
+if(!program.sum && !program.product) {
 	throw "Error. Incorrect number of flags present"
 }
 
@@ -18,11 +26,11 @@ var isNumber = function(x) {
 var numbers = args.filter(isNumber).map(parseFloat);
 var result;
 
-if(flagsPresent[0] === '--sum') {
+if(program.sum) {
 	result = numbers.reduce(function(a,b) {return a+b})
 }
 
-if(flagsPresent[0] === '--product') {
+if(program.product) {
 	result = numbers.reduce(function(a,b) {return a*b})
 }
 
